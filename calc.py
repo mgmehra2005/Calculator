8b025bfe7c68f5bdd751028e6f164960308c14d0
Matang Mehra <m2021_mehra@rediffmail.com>
2021-07-08 21:11:55 +0530

    v.1.1.1

--- calc.py
+++ calc.py
@@ -2,86 +2,80 @@ import math
 from typing import Text
 
 print("\nCALCULATOR")
-print("\n\tMADE BY : Matang Mehra")
+print("\n\tBuild By Matang Mehra")
 
 print("\n1. Addition \n2. Division \n3. Multiplication \n4. Substriction \n5. Percentage \n6. Discount Checker \n7. Square Root")
 
-print("\n\nChoose one of them : ")
-choosen_number = input()
+first = "\nChoose one of them : "
+choosen_number = input(first)
 
 #Addition
-if choosen_number == "1":    
+if choosen_number == "1":
     print("\nYou have choosed Addition.")
-    print("\nValue 1 :")
-    value1a = input()
-    print("\nValue 2 :")
-    value2a = input()
+    fa = "\nValue 1 : "
+    value1a = input(fa) 
+    sa = "\nValue 2 : "
+    value2a = input(sa)
     total_a = int(value1a)+int(value2a)
     print(f"\nTotal : {total_a}")
 
 #Division
 elif choosen_number == "2":
     print("\nYou have choosed Division.")
-    print("\nNumerator :")
-    value1d = input()
-    print("\nDenominator :")
-    Value2d = input()
+    fd = "\nNumerator : "
+    value1d = input(fd)
+    sd = "\nDenominator : "
+    Value2d = input(sd)
     total_d = int(value1d)/int(Value2d)
     print(f"\nTotal : {total_d}")
 
 #Multiplication
 elif choosen_number == "3":
     print("\nYou have choosed Multiplication.")
-    print("\nValue 1 :")
-    value1m = input()
-    print("\nValue 2 :")
-    Value2m = input()
+    fm = "\nValue 1 : "
+    value1m = input(fm)
+    sm = "\nValue 2 : "
+    Value2m = input(sm)
     total_m = int(value1m)*int(Value2m)
     print(f"\nTotal : {total_m}")
 
 #Substriction
 elif choosen_number == "4":
     print("\nYou have choosed Substriction.")
-    print("\nValue 1 :")
-    value1s = input()
-    print("\nValue 2 :")
-    Value2s = input()
+    fs = "\nValue 1 : "
+    value1s = input(fs)
+    ss = "\nValue 2 : "
+    Value2s = input(ss)
     total_s = int(value1s)-int(Value2s)
     print(f"\nTotal : {total_s}")
 
 #Percentage Calculator
 elif choosen_number == "5":
     print("\nYou have choosed Percentage.")
-    print("\nValue :")
-    value1p = input()
-    print("\nTotal :")
-    Value2p = input()
+    fp = "\nValue :"
+    value1p = input(fp)
+    sp = "\nTotal :"
+    Value2p = input(sp)
     total_p = int(value1p)/int(Value2p)*100
     print(f"\nYour Answer : {total_p} %")
 
 #Discount Checker
 elif choosen_number == "6":
     print("\nYou have choosed Discount Checker.")
-    print("\nValue :")
-    value1dc = input()
-    print("\nTotal :")
-    Value2dc = input()
+    fd = "\nValue : "
+    value1dc = input(fd)
+    sd = "\nTotal : "
+    Value2dc = input(sd)
     total_dc = 100-int(value1dc)/int(Value2dc)*100
     print(f"\nDiscount : {total_dc} %")
     
 #Squre Root
 elif choosen_number == "7":
     print("\nYou have choosen Square Root.")
-    print("\nValue :")
-    text = int(input())
+    fr = "\nValue : "
+    text = int(input(fr))
     value1sq = math.sqrt(text)
     print(f"\nSquare Root : {value1sq}")
     
 else:
     print("You have entered worng value.")
-'''
-hlo everyone this code is written by me i have added certain things in this code you can check or you have permission to impove this code. 
-
-Thank You
-Matang Mehra
-'''
